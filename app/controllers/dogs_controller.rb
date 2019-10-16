26ba6de2fa3ceceb679af2dd5bc649fb428b5ba1
alexiscash <alexiscash97@rocketmail.com>
2019-10-16 17:05:16 -0500

    done

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,36 @@
 class DogsController < ApplicationController
+    before_action :current_dog, only: [:show, :edit, :update, :destroy]
+
+    def index
+        @dogs = Dog.all 
+    end
+    
+    def new
+    
+    end
+    
+    def create
+    
+    end
+    
+    def edit
+    
+    end
+    
+    def update
+    
+    end
+    
+    def show
+    end
+    
+    def destroy
+    
+    end
+
+    private
+
+    def current_dog
+        @dog = Dog.find(params[:id])
+    end
 end
--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,50 @@
 class EmployeesController < ApplicationController
+    before_action :current_employee, only: [:show, :edit, :update, :destroy]
+
+    def index
+        @employees = Employee.all
+    end
+    
+    def new
+        @employee = Employee.new
+        @dogs = Dog.all
+    end
+    
+    def create
+        @employee = Employee.new(employees_params)
+
+        if @employee.valid?
+            @employee.save
+            redirect_to @employee
+        else
+            flash[:error] = @employee.errors.full_messages
+            redirect_to new_employee_path
+        end
+    end
+    
+    def edit
+        @dogs = Dog.all
+    end
+    
+    def update
+       @employee.update(employees_params)
+       redirect_to @employee
+    end
+    
+    def show
+    end
+    
+    def destroy
+    
+    end
+
+    private
+
+    def current_employee
+        @employee = Employee.find(params[:id])
+    end
+
+    def employees_params
+        params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :dog_id)
+    end
 end
