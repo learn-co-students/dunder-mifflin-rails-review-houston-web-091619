da120709702c2cc8cd0fced8cb3cdedbd56dd461
Fabi <fafi@Fabis-MacBook-Pro.local>
2019-10-17 09:44:55 -0500

    Done.

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,19 @@
 class DogsController < ApplicationController
+     def index
+        @dogs = Dog.all
+    end
+
+    def show
+     @dog = Dog.find(params[:id])
+    end
+
+    def new
+        @dogs = Dog.all
+    end
+    
+    def create
+        # byebug
+        @dogs = Dog.create(dogs_params)
+        redirect_to @dogs
+    end
 end
